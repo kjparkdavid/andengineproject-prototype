83fb8133ffcfa01eac0e75dbf47919600172e4f8
unknown <parkd2@TDL00685123.TDBFG.com>
2014-12-03 10:07:31 -0500

    add demo splash and touch to start

--- src/com/snowrain/gameprojectproto/InGameActivity.java
+++ src/com/snowrain/gameprojectproto/InGameActivity.java
@@ -6,7 +6,9 @@ import org.andengine.engine.handler.timer.TimerHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
@@ -51,18 +53,30 @@ public class InGameActivity extends BaseGameActivity {
 	@Override
 	public void onPopulateScene(Scene pScene,
 			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
-		 mEngine.registerUpdateHandler(new TimerHandler(2f, new ITimerCallback() 
-		    {
-		            public void onTimePassed(final TimerHandler pTimerHandler) 
-		            {
-		                mEngine.unregisterUpdateHandler(pTimerHandler);
-		                // load menu resources, create menu scene
-		                // set menu scene using scene manager
-		                // disposeSplashScene();
-		                // READ NEXT ARTICLE FOR THIS PART.
-		                SceneManager.getInstance().createMenuScene();
-		            }
-		    }));
+//		 mEngine.registerUpdateHandler(new TimerHandler(2f, new ITimerCallback() 
+//		    {
+//		            public void onTimePassed(final TimerHandler pTimerHandler) 
+//		            {
+//		                mEngine.unregisterUpdateHandler(pTimerHandler);
+//		                // load menu resources, create menu scene
+//		                // set menu scene using scene manager
+//		                // disposeSplashScene();
+//		                // READ NEXT ARTICLE FOR THIS PART.
+//		                SceneManager.getInstance().createMenuScene();
+//		            }
+//		    }));
+	//	pScene.setOnSceneTouchListener(pOnSceneTouchListener)
+		SceneManager.getInstance().getCurrentScene().setOnSceneTouchListener(new IOnSceneTouchListener() {
+			
+			@Override
+			public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+				if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_DOWN) {
+					SceneManager.getInstance().createMenuScene();
+			        return true;
+			    }
+			    return false;
+			}
+		});
 		    pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
 	
